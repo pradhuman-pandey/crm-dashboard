c2c51f48005284b36232209b75ca0afa7b28e7f9
Pradhuman Pandey <pradhumanpandeycpp@gmail.com>
2026-05-17 10:23:02 +0530

    main branch update

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -86,7 +86,7 @@ const Header = ({
             className="
               absolute
               right-2
-              top-1
+              top-2
               h-2
               w-2
               rounded-full
