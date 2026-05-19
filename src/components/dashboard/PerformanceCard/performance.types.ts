14a51386912eba526649b03d384f98598b763eb0
Pradhuman Pandey <pradhumanpandeycpp@gmail.com>
2026-05-19 07:41:31 +0530

    merge

--- src/components/dashboard/PerformanceCard/performance.types.ts
+++ src/components/dashboard/PerformanceCard/performance.types.ts
@@ -13,9 +13,5 @@ export interface PerformanceItem {
 
   description: string;
 
-<<<<<<< HEAD
-  className:string;
-=======
   className?:string;
->>>>>>> 3261a0178930779167cecddf93b7e411a83b4ee4
 }
